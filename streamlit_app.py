0f96d46fd505b01191095be2234a051437bf3cf4
MichaelLudwig <michael.ludwig3@gmail.com>
2024-07-26 14:42:30 +0200

    ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import pandas as pd
+from openai import OpenAI
+import json
 
 st.title("My new app")
-st.write(
-    "huhu"
-)
+st.write("API:", st.secrets["OPENAI_API_KEY"])
