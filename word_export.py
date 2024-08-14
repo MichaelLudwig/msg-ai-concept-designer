d0b168996ba8925968cc828644699d7f8b08a10f
MichaelLudwig <michael.ludwig3@gmail.com>
2024-08-14 18:09:38 +0200

    ,

--- word_export.py
+++ word_export.py
@@ -79,7 +79,7 @@ def export_dokument_to_word (new_title,new_header,toc_list, content, glossar):
         for line in lines:
             if line.startswith("### ") or line.startswith("#### "):
                 # Wenn eine neue Überschrift 2 gefunden wird
-                heading = line.replace("### ", "").replace("#### ", "").strip()
+                heading = line.replace("#### ", "").replace("### ", "").strip()
                 chapter_content.append({"text": heading, "style": "Header"})
             else:
                 if line.startswith("- **"):
