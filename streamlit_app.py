c448d177a6b3845019c80aa4228ac0cc69cbd1ac
MichaelLudwig <michael.ludwig3@gmail.com>
2024-08-14 15:38:40 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,9 +125,6 @@ new_doctype = newdoc_form.selectbox("Dokumenttyp",
 new_content_focus = newdoc_form.text_area("Inhaltlicher Schwerpunkt")
 new_context = newdoc_form.text_area("Kontext")
 new_stakeholder = newdoc_form.text_input("Zielgruppe", value="Technisches Fachpersonal")
-new_writing_style = newdoc_form.selectbox("Wähle den Schreibstil.", ["msg Konzept", "Fachlich", "Technisch", "Akademisch", "Sarkastisch"])
-if new_writing_style == "msg Konzept":
-    new_writing_style = "Schreibe den Text in einem formalen und strukturierten Stil, wie es in Konzepten üblich ist. Verwende präzise und sachliche Sprache mit klaren, kurzen Sätzen. Es wird eine objektive und distanzierte Haltung eingenommen. Der Text verzichtet auf persönliche Ansprache oder emotionalen Ausdruck und konzentriert sich stattdessen auf klare Darstellung von Informationen und Anweisungen. Der Text soll in der dritten Person Singular und im Präsens geschrieben sein."
 new_chapter_count = newdoc_form.slider("Anzahl der Kapitel.", min_value=1, max_value=20, value=8)
 
 new_submitted = newdoc_form.form_submit_button("Dokumentstruktur erstellen")
@@ -140,7 +137,10 @@ else:
     toc_list=[]
 
 #Sidebar Steuerelemente    
-new_word_count = st.sidebar.slider("Anzahl der Wörter pro Kapitel.", min_value=50, max_value=500, value=100, step=50)        
+new_word_count = st.sidebar.slider("Anzahl der Wörter pro Kapitel.", min_value=50, max_value=500, value=100, step=50)
+new_writing_style = newdoc_form.selectbox("Wähle den Schreibstil.", ["msg Konzept", "Fachlich", "Technisch", "Akademisch", "Sarkastisch"])
+if new_writing_style == "msg Konzept":
+    new_writing_style = "Schreibe den Text in einem formalen und strukturierten Stil, wie es in Konzepten üblich ist. Verwende präzise und sachliche Sprache mit klaren, kurzen Sätzen. Es wird eine objektive und distanzierte Haltung eingenommen. Der Text verzichtet auf persönliche Ansprache oder emotionalen Ausdruck und konzentriert sich stattdessen auf klare Darstellung von Informationen und Anweisungen. Der Text soll in der dritten Person Singular und im Präsens geschrieben sein."        
 if st.sidebar.button("Word Dokument exportieren", key="word_export"):
         if 'glossar' not in st.session_state:
             st.session_state.glossar = generate_glossar(st.session_state.kapitel_inhalt)
