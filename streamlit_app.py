7276468f71ae6ac8a389804f9f59c446ef71d7d3
MichaelLudwig <michael.ludwig3@gmail.com>
2024-08-15 10:29:15 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -131,7 +131,7 @@ if 'toc_list' in st.session_state:
 else:
     toc_list=[]
 
-#Sidebar Steuerelemente 
+#Schaltflächen für die Kapitelbearbeitung 
 st.sidebar.subheader("Kapitel Steuerelemente", divider='grey')   
 new_word_count = st.sidebar.slider("Anzahl der Wörter pro Kapitel.", min_value=50, max_value=1000, value=100, step=50)
 new_writing_style = st.sidebar.selectbox("Wähle den Schreibstil.", ["msg Konzept", "Fachlich", "Technisch", "Akademisch", "Sarkastisch"])
@@ -140,7 +140,27 @@ if new_writing_style == "msg Konzept":
 new_context = st.sidebar.text_area("Kontext")
 new_stakeholder = st.sidebar.text_input("Zielgruppe", value="Technisches Fachpersonal")
 
+#Schaltflächen für die Kapitelstruktur bearbeitung
+#newchapter_form = st.sidebar.form("newchapter_form_key")
+#newchapter_position = newchapter_form.slider("Position für neues Kapitel", min_value=1, max_value=len(toc_list), value=1)
+#newchapter_title = newchapter_form.text_input("Kaitel Überschrift")
+#newchapter_submitted = newchapter_form.form_submit_button("Kapitel einfügen")
+#if newchapter_submitted:
+#    toc_list.insert(newchapter_position, 
+#                    {
+ #                       "title":newchapter_title,
+#                        "help_text":"",
+ #                       "prompt_text":"",
+ #                   }
+ #                   )
+ #   st.session_state.toc_list = toc_list
+ #   st.session_state.kapitel_info.insert(newchapter_position, "")
+ #   st.session_state.kapitel_prompt.insert(newchapter_position, "")
+ #   st.session_state.prompt_area.insert(newchapter_position, "")
+ #   st.session_state.kapitel_inhalt.insert(newchapter_position, "")
 
+
+#Schaltflächen für den Word Export
 st.sidebar.subheader("Word Export", divider='grey')
 
 if st.sidebar.button("Glossar (re)generieren", key="Glossar"):
@@ -198,5 +218,5 @@ if 'glossar' in st.session_state:
     st.write(st.session_state.glossar)
 
 
-st.write(st.session_state.toc_list)
+#st.write(st.session_state.toc_list)
 
