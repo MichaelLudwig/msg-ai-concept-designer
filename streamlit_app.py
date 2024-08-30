4de2da419d53f9752f9baec9dc9f9bdd562f1e50
MichaelLudwig <michael.ludwig3@gmail.com>
2024-08-30 13:25:00 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,11 @@ st.sidebar.subheader("Neues Dokument", divider='grey')
 newdoc_form = st.sidebar.form("newdoc_form_key")
 new_title = newdoc_form.text_input("Dokumenttitel")
 new_doctype = newdoc_form.selectbox("Dokumenttyp",
-    ["Fachkonzept", "IT-Konzept", "Anforderungskonzept", "Marktanalyse",
+    ["Fachkonzept", "IT-Konzept", "Anforderungskonzept", 
+    # "Marktanalyse",
     "Architekturkonzept", "Infrastrukturkonzept", "Migrationskonzept", "Deploymentkonzept", "Testkonzept", "Backupkonzept", "Schnittstellenkonzept", 
     "Sicherheitskonzept", "Rollen- und Rechtekonzept",  
-    "Datenschutzkonzept", "Datenschutzerklärung", "Schutzbedarfsfeststellung (nach BSI Grundschutz)", "Auftragsverarbeitungs-Vertrag", "Verzeichnis von Verarbeitungstätigkeiten", "Datenschutzfolgeabschätzung",
+    #"Datenschutzkonzept", "Datenschutzerklärung", "Schutzbedarfsfeststellung (nach BSI Grundschutz)", "Auftragsverarbeitungs-Vertrag", "Verzeichnis von Verarbeitungstätigkeiten", "Datenschutzfolgeabschätzung",
     "Changemanagementkonzept", "Löschkonzept", "Verschlüsselungskonzept", "Datensicherungskonzept", 
     "Betriebsführungskonzept", "Betriebsführungshandbuch", "Notfallkonzept", 
     "Dokumentationskonzept", "Risikomanagementkonzept", "Compliancekonzept", "Qualitätsmanagementkonzept",
