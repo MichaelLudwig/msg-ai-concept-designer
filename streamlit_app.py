e3e50dfa28527326b08b69436e38b107a00151ba
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-03 14:55:58 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ new_word_count = st.sidebar.slider("Anzahl der Wörter pro Kapitel.", min_value=
 new_writing_style = st.sidebar.selectbox("Wähle den Schreibstil.", ["msg Konzept", "Fachlich", "Technisch", "Akademisch", "Sarkastisch"])
 if new_writing_style == "msg Konzept":
     new_writing_style = "Schreibe den Text in einem formalen und strukturierten Stil, wie es in Konzepten üblich ist. Verwende präzise und sachliche Sprache mit klaren, kurzen Sätzen. Es wird eine objektive und distanzierte Haltung eingenommen. Der Text verzichtet auf persönliche Ansprache oder emotionalen Ausdruck und konzentriert sich stattdessen auf klare Darstellung von Informationen und Anweisungen. Der Text soll in der dritten Person Singular und im Präsens geschrieben sein."        
-new_context = st.sidebar.text_area("Kontext",help="So wird dieses Feld verwendet")
+new_context = st.sidebar.text_area("Kontext")
 new_stakeholder = st.sidebar.text_input("Zielgruppe", value="Technisches Fachpersonal")
 
 
