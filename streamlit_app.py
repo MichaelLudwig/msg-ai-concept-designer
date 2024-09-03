7a76709673d57adca3b0b6bf6368c0e17aeb4f4e
MichaelLudwig <michael.ludwig3@gmail.com>
2024-09-03 18:07:51 +0200

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -95,7 +95,7 @@ if new_submitted:
     st.session_state.kapitel_info = [""] * len(toc_list)
     st.session_state.kapitel_inhalt = [""] * len(toc_list)
     st.session_state.kapitel_prompt = [""] * len(toc_list)
-    st.session_state.prompt_area = [""] * len(toc_list)
+    # st.session_state.prompt_area = [""] * len(toc_list)
     st.session_state.glossar = ""
 
     
