442bb8862a9b702ee97b3ef604803bae3a960d8f
MichaelLudwig <michael.ludwig3@gmail.com>
2024-10-17 16:57:23 +0200

    .

--- pages/chat_bot.py
+++ pages/chat_bot.py
@@ -23,21 +23,39 @@ user_prompt = st.chat_input("Frag GPT-4o-mini...")
 
 if user_prompt:
     # add user's message to chat and display it
-    st.chat_message("user").markdown(user_prompt)
     st.session_state.chat_history.append({"role": "user", "content": user_prompt})
 
-    # send user's message to GPT-4o and get a response
-    response = client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[
-            {"role": "system", "content": "Du bist ein hilfreicher Assistent"},
-            *st.session_state.chat_history
-        ]
-    )
+    # Display user message in chat message container
+    #st.chat_message("user").markdown(user_prompt)
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
 
-    assistant_response = response.choices[0].message.content
+    # Display assistant response in chat message container
+    #Version ohne stream, also die Antwort wird komplett ausgegeben ------------------------------
+    # send user's message to GPT-4o and get a response
+    #response = client.chat.completions.create(
+    #    model=st.session_state["openai_model"],
+    #    messages=[
+    #        {"role": "system", "content": "Du bist ein hilfreicher Assistent"},
+    #        *st.session_state.chat_history
+    #    ]
+    #)
+    #assistant_response = response.choices[0].message.content
+
+    #Version mit stream, die Antwort wird ausgegeben, als würde der Chatbot sie live schreiben
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.chat_history
+            ],
+            stream=True,
+        )
+        assistant_response = st.write_stream(stream)    
     st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
 
-    # display GPT-4o's response
-    with st.chat_message("assistant"):
-        st.markdown(assistant_response)
+    #Version ohne stream, also die Antwort wird komplett ausgegeben ------------------------------
+    # # display GPT-4o's response
+    #with st.chat_message("assistant"):
+    #    st.markdown(assistant_response)
