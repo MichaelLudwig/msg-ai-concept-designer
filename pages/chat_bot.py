dc5b5935aa5b2bffed86f22d713033849487818c
MichaelLudwig <michael.ludwig3@gmail.com>
2024-10-17 16:37:14 +0200

    .

---
+++ pages/chat_bot.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import openAI_API
+
+client = openAI_API.get_oai_client()
+openAI_model = "gpt-4o-mini"
+
+# initialize chat session in streamlit if not already present
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# streamlit page title
+st.title("🤖 Azure OpenAI GPT-4o-mini ChatBot")
+st.text(st.session_state.ai_api_info)
+
+# display chat history
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+# input field for user's message
+user_prompt = st.chat_input("Frag GPT-4o-mini...")
+
+if user_prompt:
+    # add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
+    st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+    # send user's message to GPT-4o and get a response
+    response = client.chat.completions.create(
+        model=openAI_model,
+        messages=[
+            {"role": "system", "content": "Du bist ein hilfreicher Assistent"},
+            *st.session_state.chat_history
+        ]
+    )
+
+    assistant_response = response.choices[0].message.content
+    st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+
+    # display GPT-4o's response
+    with st.chat_message("assistant"):
+        st.markdown(assistant_response)
